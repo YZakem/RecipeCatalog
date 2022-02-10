2b0eba645432a73369285e8a19b3919f653b0a76
unknown <ye.zakem@gmail.com>
2022-02-10 13:36:20 -0500

    initial commit

---
+++ src/test/java/com/zakem/RecipeCatalog/RecipeCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakem.RecipeCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
